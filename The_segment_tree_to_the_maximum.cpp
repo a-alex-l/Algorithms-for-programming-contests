4c3d3342ab5e61aef487fef0a05a21873244404c
a-alex-l <40340659+a-alex-l@users.noreply.github.com>
2019-10-18 21:10:10 +0300

    Update The_segment_tree_to_the_maximum.cpp

--- The_segment_tree_to_the_maximum.cpp
+++ The_segment_tree_to_the_maximum.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-const int INF = 1e+9;
+const long long INF = 1e+18;
 
-vector<int> tree;
+vector<long long> tree;
 
-int get_max(int i, int left, int right, int inleft, int inright) {
+long long get_max(int i, int left, int right, int inleft, int inright) {
     if (inleft <= left && right <= inright) 
         return tree[i];
     if (right <= inleft || inright <= left)
@@ -17,7 +17,7 @@ int get_max(int i, int left, int right, int inleft, int inright) {
     return max(get_max(i * 2 + 1, left, mid, inleft, inright), get_max(i * 2 + 2, mid, right, inleft, inright));
 }
 
-int put_elem(int i, int left, int right, int pos, int val) {
+long long put_elem(int i, int left, int right, int pos, long long val) {
     if (pos == left && left == right - 1)
         return tree[i] = val;
     if (pos < left || right <= pos)
@@ -33,7 +33,7 @@ int main() {
     cin >> n;
     tree.resize(4 * n);
     for (int i = 0; i < n; i++) {
-        int a;
+        long long a;
         cin >> a;
         put_elem(0, 0, n, i, a);
     }
@@ -42,7 +42,7 @@ int main() {
     cin >> q;
     for (int i = 0; i < q; i++) {
         char x;
-        int a, b;
+        long long a, b;
         cin >> x >> a >> b;
         if (x == 's')
             cout << get_max(0, 0, n, a - 1, b) << " ";
